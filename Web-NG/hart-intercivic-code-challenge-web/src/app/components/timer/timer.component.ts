93afdf426478dba48e110c89ad5df4dfe7feed5a
mmakarwich10 <mist_shaker@yahoo.com>
2023-02-23 15:37:41 -0600

    Implemented Timer functionality.
    + Added RxJS package.

--- Web-NG/hart-intercivic-code-challenge-web/src/app/components/timer/timer.component.ts
+++ Web-NG/hart-intercivic-code-challenge-web/src/app/components/timer/timer.component.ts
@@ -1,11 +1,49 @@
 import { Component } from "@angular/core";
+import { Subscription, timer } from "rxjs";
 
 @Component({
     selector: 'hicc-timer',
     templateUrl: './timer.component.html'
 })
 export class TimerComponent {
-    public hours: number = 0;
-    public minutes: number = 2;
-    public seconds: number = 0;
+    public hours: number = 1;
+    public minutes: number = 0;
+    public seconds: number = 1;
+
+    private _timerSub: Subscription = new Subscription();
+
+    public startTimer() {
+        const oTimer = timer(0, 1000);
+        this._timerSub = oTimer.subscribe(x => {
+            if (this.seconds === 0 && this.minutes === 0 && this.hours === 0) {
+                this._timerSub.unsubscribe();
+            } else {
+                this._tickSecondDown();
+            }
+        });
+    }
+
+    private _tickSecondDown() {
+        if (this.seconds === 0) {
+            this._tickMinuteDown();
+            this.seconds = 59;
+        } else {
+            this.seconds--;
+        }
+    }
+
+    private _tickMinuteDown() {
+        if (this.minutes === 0) {
+            this._tickHoursDown();
+            this.minutes = 59;
+        } else {
+            this.minutes--;
+        }
+    }
+
+    private _tickHoursDown() {
+        if (this.hours > 0) {
+            this.hours--;
+        }
+    }
 }
