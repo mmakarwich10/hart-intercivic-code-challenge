5c7f2539687e57ce7daab34fc5c3a7c1066962a5
mmakarwich10 <mist_shaker@yahoo.com>
2023-02-24 10:21:48 -0600

    Added notification API service for when timer is finished.
    + Added Pause Timer functionality.

--- Web-NG/hart-intercivic-code-challenge-web/src/app/components/timer/timer.component.ts
+++ Web-NG/hart-intercivic-code-challenge-web/src/app/components/timer/timer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Subscription, timer } from "rxjs";
+import { NotificationApiService } from "src/app/services/notification-api.service";
 import { TimerApiService } from "src/app/services/timer-api.service";
 
 @Component({
@@ -11,19 +12,38 @@ export class TimerComponent implements OnInit {
     public minutes: number = 0;
     public seconds: number = 0;
 
+    public successfulNotification: boolean = false;
+    public timerIsRunning: boolean = false;
+
     private _timerSub: Subscription = new Subscription();
 
-    constructor(private _timerApiService: TimerApiService) {}
+    constructor(
+        private _timerApiService: TimerApiService,
+        private _notificationApiService: NotificationApiService
+    ) {}
 
     public startTimer() {
+        this.successfulNotification = false;
+
         const oTimer = timer(0, 1000);
+        this.timerIsRunning = true;
         this._timerSub = oTimer.subscribe(x => {
             if (this.seconds === 0 && this.minutes === 0 && this.hours === 0) {
-                this._timerSub.unsubscribe();
+                this.pauseTimer();
+                this._notificationApiService.sendTimerIsFinished().subscribe(response => {
+                    if (response != undefined && response != null) {
+                        this.successfulNotification = response;
+                    }
+                });
             } else {
                 this._tickSecondDown();
             }
         });
+    }
+
+    public pauseTimer() {
+        this._timerSub.unsubscribe();
+        this.timerIsRunning = false;
     }
 
     public ngOnInit(): void {
